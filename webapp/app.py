3dbdebc668243205bb91f87d5ea72773fed2edae
Will Cotta <wjcotta@mit.edu>
2019-11-12 20:35:58 -0600

    Added additional pages for appliances

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello World'
+	return render_template('index.html')
+
+@app.route('/ClothesWasher')
+def ClothesWasher():
+	return 'Clothes Washer'
+
+@app.route('/ClothesDryer/')
+def ClothesDryer():
+	return 'Clothes Dryer'
+
+@app.route('/Dishwasher')
+def Dishwasher():
+	return 'Dishwasher'
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
